cf33bcd6cba7cc6f19b9cd7c8d99c593493b52b5
Prathamesh Shingne <prathamshingne@gmail.com>
2023-11-03 00:23:43 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-from app import app
+# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0',port='6000',debug=True)
