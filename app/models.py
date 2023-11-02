8463b91617333dc7173312a07546765b558639f4
Prathamesh Shingne <prathamshingne@gmail.com>
2023-11-03 00:21:20 +0530

    Delete app directory

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-import datetime
-
-class Todo(db.Document):
-    content = db.StringField(required=True, max_length=20)
-    time = db.DateTimeField(default=datetime.datetime.now())
-    status = db.IntField(default=0)
