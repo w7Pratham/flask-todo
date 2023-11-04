7061781553881e1ae3e914f37ac7fafd96c345a7
w7Pratham <prathamshingne@gmail.com>
2023-11-05 02:27:01 +0530

    asdff

--- temp.sh
+++ temp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker image build -t flask_docker:v1 . && docker run -p 5001:5001 -dt flask_docker:v1
+docker rmi flask_docker:v1 && docker stop flskdeployed
+
+docker image build -t flask_docker:v1 . && docker run --name flskdeployed -p  5001:5001 -dt flask_docker:v1
