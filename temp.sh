bfbfeb11db6e9d5b165b54678d10857ade258cca
w7Pratham <prathamshingne@gmail.com>
2023-11-05 16:10:49 +0530

    fin

--- temp.sh
+++ temp.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker rmi flask_docker:v1 && docker stop flskdeployed
-
-docker image build -t flask_docker:v1 . && docker run --name flskdeployed -p  5001:5001 -dt flask_docker:v1
+python3 /root/app.py
